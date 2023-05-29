d38ac3c1139fc9544e27fa314550d1a4dc8b7e7e
Jannis Konrad <kabel42@gmail.com>
2023-05-29 15:31:19 +0200

    fix linker warnings

--- src/ch32v003/startup/startup_ch32v00x.c
+++ src/ch32v003/startup/startup_ch32v00x.c
@@ -234,3 +234,41 @@ int _write(int fd, const char *buf, int size)
 	}
 	return size;
 }
+
+int _close(int file) {
+  return -1;
+}
+
+int _fstat(int file, struct stat *st) {
+  st->st_mode = S_IFCHR;
+
+  return 0;
+}
+
+int _isatty(int file) {
+  return 1;
+}
+
+int _lseek(int file, int ptr, int dir) {
+  return 0;
+}
+
+void _exit(int status) {
+  __asm("EBREAK");
+}
+
+void _kill(int pid, int sig) {
+  return;
+}
+
+int _getpid(void) {
+  return -1;
+}
+
+int _read (int file, char * ptr, int len) {
+	return 0;
+}
+
+void debug() {
+	__asm("EBREAK");
+}
--- src/ch32v003/startup/startup_ch32v00x.h
+++ src/ch32v003/startup/startup_ch32v00x.h
@@ -6,6 +6,16 @@ void DefaultIRQHandler( void ) __attribute__((section(".text.vector_handler"))) 
 #define DMDATA0 ((volatile uint32_t*)0xe00000f4)
 #define DMDATA1 ((volatile uint32_t*)0xe00000f8)
 
+#include <sys/stat.h>
+
 int _write(int fd, const char *buf, int size) __attribute__((used));
+int _close(int file) __attribute__((used));
+int _fstat(int file, struct stat *st) __attribute__((used));
+int _isatty(int file) __attribute__((used));
+int _lseek(int file, int ptr, int dir) __attribute__((used));
+void _exit(int status) __attribute__((used));
+void _kill(int pid, int sig) __attribute__((used));
+int _getpid(void) __attribute__((used));
+int _read (int file, char * ptr, int len) __attribute__((used));
 
 #endif
